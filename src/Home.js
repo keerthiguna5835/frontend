d0db057830f0fdb4fbfc76ad47a90d642d30242c
keerthiguna5835 <keerthiguna5835@gmail.com>
2024-03-11 04:36:29 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmxnqt8YC8XLqj7RkdzoE_Ysk3zukTK56aBcpRyTuaUlObysmavyeDt1xIaKgiqGwx_EA&usqp=CAU" alt="Welcome picture" />
+    </div>
+  )
+}
